4a23cb6fa1ca05f48ce5c9b48e3c817010a4c59e
Andrey Vetrov <a.vetrov@gmail.com>
2021-02-15 23:15:13 +0300

    MainPage correction

--- src/pages/main/MainPage.jsx
+++ src/pages/main/MainPage.jsx
@@ -3,15 +3,17 @@ import { Link } from "react-router-dom";
 import {NAVIGATION_URL} from "../../__data__/constants";
 import alphabet from './images/alphabet.svg';
 import paint from './images/paint.svg'
+import style from './MainPage.module.css'
+
 
 function MainPage() {
     return (
-        <nav>
-            <Link to={NAVIGATION_URL.PAINT}>
-                <img src={alphabet} />
+        <nav className={style.nav}>
+            <Link to={NAVIGATION_URL.PAINT} className={style.link}>
+                <img src={alphabet} className={style.alphabet}/>
             </Link>
-            <Link to={NAVIGATION_URL.ALPHABET}>
-                <img src={paint} />
+            <Link to={NAVIGATION_URL.ALPHABET} className={style.link}>
+                <img src={paint} className={style.paint}/>
             </Link>
         </nav>
     );
