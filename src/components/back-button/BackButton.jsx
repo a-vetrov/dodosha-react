9281c142bf50428049eead526c95bee90b16d8e5
Ветров Андрей Алексеевич <Vetrov.A.Al@sberbank.ru>
2021-03-29 15:38:55 +0300

    Back button correction

--- src/components/back-button/BackButton.jsx
+++ src/components/back-button/BackButton.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
-import { Link } from "react-router-dom";
+import React, {useMemo} from 'react';
+import { Link, useLocation } from "react-router-dom";
+
 import style from './BackButton.module.css'
 import {NO_GRAPHICS_MODE} from "../../__data__/constants";
 
+const calculateUpPathName = pathname => {
+    const arr = pathname.split('/')
+    const last = arr.pop()
+
+    if (arr.length && !last)
+        arr.pop()
+
+    return arr.join('/')
+}
+
 const BackButton = () => {
 
+    const {pathname} = useLocation()
+
+    const upPathName = useMemo(() => calculateUpPathName(pathname), [pathname])
+
     if (NO_GRAPHICS_MODE)
-        return <Link to={'.'} >Назад</Link>
+        return <Link to={upPathName}>Назад</Link>
 
     return (
         <Link to={'.'} className={style.link} />
