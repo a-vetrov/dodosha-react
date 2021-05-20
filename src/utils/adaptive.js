cb7c7f2e6c1df13202b62133b51458ba4a19881c
Ветров Андрей Алексеевич <Vetrov.A.Al@sberbank.ru>
2021-05-20 12:38:25 +0300

    Adaptive utils added

---
+++ src/utils/adaptive.js
@@ -0,0 +1,18 @@
+import _ from 'lodash'
+
+
+/* from modernizr */
+function isTouchDevice () {
+    if (('ontouchstart' in window) || (window.DocumentTouch && document instanceof window.DocumentTouch)) {
+        return true
+    }
+
+    return window.matchMedia('(touch-enabled),(-webkit-touch-enabled),(-moz-touch-enabled),(-o-touch-enabled),(-ms-touch-enabled),(heartz)').matches
+}
+
+/* Touch | No touch */
+export const isTouchable = _.memoize(isTouchDevice)
+
+export const isNotTouchable = _.memoize(() => !isTouchable())
+
+export const applyForTouchable = (value, defaultValue) => isTouchable() ? value : defaultValue
