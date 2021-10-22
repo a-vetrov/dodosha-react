a1095eb1aec0ea663c13d08d043248866fb75cc5
Ветров Андрей Алексеевич <Vetrov.A.Al@sberbank.ru>
2021-10-22 10:02:01 +0300

    Clouds finished

--- src/components/header/Clouds.ts
+++ src/components/header/Clouds.ts
@@ -1,8 +1,12 @@
 import style from "./MainHeader.module.css";
 import cloudSrc from "./cloud.svg";
 import gsap from 'gsap'
+import _ from "lodash";
 
 type TimerId = ReturnType<typeof setTimeout> | undefined
+
+const VELOCITY = 30
+const CLOUD_WIDTH = 270
 
 export default class Clouds {
 
@@ -18,23 +22,40 @@ export default class Clouds {
         this.container.removeChild(cloud)
     }
 
+    createAnimation = (cloud: HTMLImageElement) => gsap.to(cloud, {
+        x: this.width,
+        opacity: 0,
+        scaleX: 0.1,
+        scaleY: 0.1,
+        duration: this.width / VELOCITY,
+        ease: 'none',
+        onComplete: this.onCloudComplete,
+        onCompleteParams: [cloud]
+    })
+
+    registerNextAdd = () => {
+        const gap = _.random(CLOUD_WIDTH, 3 * CLOUD_WIDTH)
+        const timeout = gap / VELOCITY
+        this.timer = setTimeout(this.addCloud, timeout * 1000)
+    }
+
     addCloud = () => {
         const cloud: HTMLImageElement = document.createElement('img')
         cloud.className = style.cloud
         cloud.src = cloudSrc
         cloud.alt = ''
         this.container.appendChild(cloud)
+        this.createAnimation(cloud)
+        this.registerNextAdd()
+    }
 
-        const animation = gsap.to(cloud, {
-            x: '+500',
-            opacity: 0,
-            scaleX: 0.1,
-            scaleY: 0.1,
-            duration: 3,
-            ease: 'power1.in',
-            onComplete: this.onCloudComplete,
-            onCompleteParams: [cloud]
+    get width(): number {
+        return window.innerWidth
+    }
+
+    destroy = () => {
+        this.container.childNodes.forEach((item) => {
+            gsap.getTweensOf(item).forEach((t) => t.kill())
         })
-
     }
 }
--- src/components/header/MainHeader.tsx
+++ src/components/header/MainHeader.tsx
@@ -8,18 +8,23 @@ const MainHeader = () => {
     const ref = useRef<HTMLDivElement>(null)
 
     useEffect(() => {
+        let clouds: Clouds | undefined
+
         if (ref.current) {
-            const clouds = new Clouds(ref.current)
+            clouds = new Clouds(ref.current)
+        }
+
+        return () => {
+            clouds?.destroy()
         }
     }, [])
 
 
     return (
-        <header className={style.header}>
+        <header className={style.header} >
             <div ref={ref} />
         </header>
     );
 };
-
 
 export default MainHeader;
